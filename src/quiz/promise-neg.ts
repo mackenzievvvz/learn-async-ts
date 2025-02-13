18206fccb1833dea0028bde54930681ca5be05aa
mackenzievvvz <43710645+mackenzievvvz@users.noreply.github.com>
2025-02-13 12:42:24 -0500

    neg

--- src/quiz/promise-neg.ts
+++ src/quiz/promise-neg.ts
@@ -3,3 +3,30 @@ const array2D_3 = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function hasNegative(arr: number[]): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        console.log('hasNegative called ... ');
+        if(arr.length === 0) {
+            reject('Cannot check an empty array');
+        }
+        if(arr.filter((num) => num < 0)) {
+            resolve(true);
+        } else {
+            reject(false);
+        }
+    });
+}
+
+const promises : Promise<boolean>[] = [];
+for (let i = 0; i < array2D_3.length; i++) {
+    promises.push(hasNegative(array2D_3[i]));
+}
+
+Promise.any(promises)
+  .then((response) => {
+      console.log(response);
+  })
+  .catch((error: any) => {
+      console.error(`Failed to fetch: ${error}`);
+  });
