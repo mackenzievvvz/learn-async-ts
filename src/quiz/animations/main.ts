4c3c8861b4f63564595ca7b9841b443f016fffa4
mackenzievvvz <43710645+mackenzievvvz@users.noreply.github.com>
2025-02-13 15:29:58 -0500

    animation

--- src/quiz/animations/main.ts
+++ src/quiz/animations/main.ts
@@ -13,49 +13,49 @@ const alice10 = document.querySelector<HTMLElement>("#alice1");
 const alice20 = document.querySelector<HTMLElement>("#alice2");
 const alice30 = document.querySelector<HTMLElement>("#alice3");
 
-if(alice10 && alice20 && alice30) {
-  // Promise chain  
-  alice10.animate(aliceTumbling1, aliceTiming1).finished  
-    .then(() => {
-        return alice20
-                .animate(aliceTumbling1, aliceTiming1)
-                .finished;     
-    })
-    .then(() => {
-      return alice30
-              .animate(aliceTumbling1, aliceTiming1)
-              .finished;
-    })
-    .catch((err) => alert(`Error when promising ... ${err.message}`));
-}
-else{
-  console.warn("#alice not found");
-}
+// if(alice10 && alice20 && alice30) {
+//   // Promise chain  
+//   alice10.animate(aliceTumbling1, aliceTiming1).finished  
+//     .then(() => {
+//         return alice20
+//                 .animate(aliceTumbling1, aliceTiming1)
+//                 .finished;     
+//     })
+//     .then(() => {
+//       return alice30
+//               .animate(aliceTumbling1, aliceTiming1)
+//               .finished;
+//     })
+//     .catch((err) => alert(`Error when promising ... ${err.message}`));
+// }
+// else{
+//   console.warn("#alice not found");
+// }
 
-async function animateAlice10() : Promise<void> {
+async function animateAlice10() : Promise<Animation> {
   if (alice10) {
-    alice10.animate(aliceTumbling1, aliceTiming1)
-              .finished;
+    console.log("animate 10");
+    return alice10.animate(aliceTumbling1, aliceTiming1).finished;
   } else {
     throw("potnetially null");
   }
 }
 
-async function animateAlice20() : Promise<void> {
+async function animateAlice20() : Promise<Animation> {
   if (alice20) {
     await animateAlice10();
-    alice20.animate(aliceTumbling1, aliceTiming1)
-              .finished;
+    console.log("animate 20");
+    return alice20.animate(aliceTumbling1, aliceTiming1).finished;
   } else {
     throw("potnetially null");
   }
 }
 
-async function animateAlice30() : Promise<void> {
+async function animateAlice30() : Promise<Animation> {
   if (alice30) {
     await animateAlice20();
-    alice30.animate(aliceTumbling1, aliceTiming1)
-              .finished;
+    console.log("animate 30");
+    return alice30.animate(aliceTumbling1, aliceTiming1).finished;
   } else {
     throw("potnetially null");
   }
@@ -76,4 +76,4 @@ animateAlice30();
 //                 alice30.animate(aliceTumbling1, aliceTiming1);
 //             })
 //     });
-  
+console.log("end of program");
